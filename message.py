b8d66a873a10834c76971d309d44954ebb5d32ca
Lyle Hubbard <lhubbard@pdx.edu>
2021-07-06 15:37:43 -0700

    message grabs kv pair to describe inputs, outputs for hyperparams

--- message.py
+++ message.py
@@ -20,7 +20,10 @@ import torch.nn as nn
 
 
 
-
+def seek(target: str, mp: dict):
+  for key in mp:
+    if target in key:
+      return key, mp[key]
 class Msg:
   """contains data payloads describing single primitive blocks, very open to change"""
   def __init__(self, msg):
@@ -38,20 +41,22 @@ class Msg:
     exec("msg = " + self.payload, globals(), locals()) 
     
     module_builder_string = ""
+    
     if msg["isNative"]:
-      module_builder_string += "nn." + name + "("
-
+      module_builder_string += "nn." + msg.name + "("
+    
     if msg["isParameter"]:
-      
-      in_key = str(list(msg.hyper.keys()))
-      module_builder_string += in_key + " = " + msg.hyper[in_key] + ", "
-      out_key = str(list(msg.hyper.keys()))
-      module_builder_string += out_key + " = " + msg.hyper[out_key] + ", "
+      in_kv_pair = seek("in_", msg.hyper)
+      module_builder_string += in_kv_pair[0] + " = " + in_kv_pair[1] + ", "
 
-    for hparam in msg.hyper:
+      out_kv_pair = seek("out_", msg.hyper)
+      module_builder_string += out_kv_pair[0] + " = " + out_kv_pair[1] + ", "
+
+    for hparam in msg.hyper.keys():
       module_builder_string += hparam + " = " + msg.hyper[hparam] + ", "
 
     module_builder_string = module_builder_string[:-2] + ")"
-    exec("obj = " + module_builder_string, globals(), locals())
 
+
+    exec("obj = " + module_builder_string, globals(), locals())
     return obj
