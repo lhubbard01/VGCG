625c107c0a592567a0de0dc096565fd0f3fc6107
Lyle Hubbard <lhubbard@pdx.edu>
2021-07-06 15:24:09 -0700

    intermediate representation file between graphical description and computational graph implementation

---
+++ IR.py
@@ -0,0 +1,19 @@
+class ModuleIntermediateRepr:
+
+  def __init__(self, name, from, to):
+    """accepts a json of the form 
+    {
+      "name" : symbol.
+      "from" : inbounds,
+      "to"   : outbounds
+      }"""
+    self.name = symbol
+    self.from = from
+    self.to = to
+
+
+
+  
+
+
+
