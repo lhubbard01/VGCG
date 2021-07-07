ec22c19c137976ffb49908408c463c9c91bdd3b0
Lyle Hubbard <lhubbard@pdx.edu>
2021-07-07 10:39:02 -0700

    validation for python-side symbols in IR

--- src/pydev/src/IR.py
+++ src/pydev/src/IR.py
@@ -1,15 +1,54 @@
-class ModuleIntermediateRepr:
+import re
+def isValid(name):
+  """checks the validity of a name as a candidate for pythonic symbol table"""
+  if !name[0].isalpha() or name[0] != "_":
+    return False
+  elif re.match(r"\s", name):
+    return False
 
-  def __init__(self, name, from, to):
-    """accepts a json of the form 
-    {
-      "name" : symbol.
-      "from" : inbounds,
-      "to"   : outbounds
-      }"""
-    self.name = symbol
-    self.from = from
-    self.to = to
+  elif re.match(r"\D\W", name):
+    return False
+
+  elif name in ["in", "return", "self", "for", "zip", "import"]:
+    return False
+  
+  return True
+
+class xBound:
+  """renders readable the connection information, hopefully. 
+  Ensures strictly positive integer params, python viable names."""
+  
+
+  def __init__(self, name: str, count: int):
+
+    if not isValid(name):
+      raise ValueError(f"name MUST be python-valid. received {name}")
+    self.name = name
+    
+    if count <= 0:
+      raise ValueError("count must be a positive integer, representing the output vector dimension")
+
+    self.count = count
+
+
+  def __repr__(self):
+    return {"name": self.name, "count": self.count}
+
+  def __str__(self):
+    return str(self)
+
+
+class ModuleIntermediateRepr:
+  """Contains information describing a module """
+  def __init__(self, name, inbound: List[xBound], outbound: List[xBound], isNative: bool isParametric: bool, hypers: dict, title: str = None):
+    self.isNative = isNative
+    self.isParametric = isParametric
+    self.name = name
+    self.title = (title if title else name)
+    # these are mappings of {name:name, count:count}
+    self.inbound = inbound
+    self.outbound = outbound
+    self.hyperparameters = hypers
 
 
 
