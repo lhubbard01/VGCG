b8bfd13ecde1afea0ef093f98c0c3ff315f23a7a
Lyle Hubbard <lhubbard@pdx.edu>
2021-07-15 10:40:01 -0700

    message now purely holds data and manipulates presentation

--- src/pydev/src/message.py
+++ src/pydev/src/message.py
@@ -28,7 +28,6 @@ class Msg:
   """contains data payloads describing single primitive blocks, very open to change"""
   def __init__(self, msg):
     self.payload = msg
-    self.obj = self.proc()
 
   def proc(self):
     """The payload comes in as a string of some mapping, following some
@@ -38,25 +37,7 @@ class Msg:
     after the execution of that statement. The object is then constructed from
     these parameters NOTE not safe for production environments!! think sql injections.
     """
-    exec("msg = " + self.payload, globals(), locals()) 
     
-    module_builder_string = ""
-    
-    if msg["isNative"]:
-      module_builder_string += "nn." + msg.name + "("
-    
-    if msg["isParameter"]:
-      in_kv_pair = seek("in_", msg.hyper)
-      module_builder_string += in_kv_pair[0] + " = " + in_kv_pair[1] + ", "
+    exec("self.as_dict = " + self.payload, globals(), locals())
+    return self.as_dict
 
-      out_kv_pair = seek("out_", msg.hyper)
-      module_builder_string += out_kv_pair[0] + " = " + out_kv_pair[1] + ", "
-
-    for hparam in msg.hyper.keys():
-      module_builder_string += hparam + " = " + msg.hyper[hparam] + ", "
-
-    module_builder_string = module_builder_string[:-2] + ")"
-
-
-    exec("obj = " + module_builder_string, globals(), locals())
-    return obj
