7fe67c0115431e229be658dd7b6c0ac8926eff87
Lyle Hubbard <lhubbard@pdx.edu>
2021-07-19 02:49:04 -0700

    cleaned up formatting etc

--- src/pydev/src/ModelDataReader_Pipe.py
+++ src/pydev/src/ModelDataReader_Pipe.py
@@ -1,18 +1,28 @@
-import os
-from message import Msg
 import json
+import os
 import select
 import struct
+
+from message import Msg
 from gen import *
+
 IPC_FIFO_MODEL = "model_pipe"
+
+
+
+
+
+
 def get_message(fifo, nbytes = 1024):
   return os.read(fifo, nbytes)
+
 class ModelIPC_Writer:
+
   def __init__(self,loc):
     os.mkfifo(IPC_FIFO_MODEL)
-    self.loc=loc
+    
+    self.loc   = loc
     self.cache = ModelCache()
-
 
     try:
       self.R=os.open(os.path.join(loc, IPC_FIFO_MODEL), os.O_RDONLY | os.O_NONBLOCK)
@@ -21,12 +31,9 @@ class ModelIPC_Writer:
       try:
         poll = select.poll()
         poll.register(self.R, select.POLLIN)
-        
-
-
-
 
         print("registered to the poll, entering loop")
+
         try:
           while True:
             if (self.R, select.POLLIN) in poll.poll(1000):
@@ -43,19 +50,24 @@ class ModelIPC_Writer:
 
         except Exception as e:
           raise e
+
       except Exception as e:
         raise e
+
       finally: 
         print("exitting, unregistering model pipe")
         poll.unregister(self.R)
+
     except Exception as e: 
       print(e)
       raise e
+
     finally:
       os.remove(os.path.join(self.loc, IPC_FIFO_MODEL))
       print("done model ipc")
 
   def proc_json(self, msg):
+    #exists to process nested string versions of parameters from frontend packets
     try:
       return json.loads(msg)
 
