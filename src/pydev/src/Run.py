8c07d1296f3f35171f1e51b0b6cb3c4355f26f9f
Lyle Hubbard <lhubbard@pdx.edu>
2021-08-14 18:15:47 -0700

    moved files to same directory

--- src/pydev/src/Run.py
+++
@@ -1,9 +0,0 @@
-FOR_BATCH    = "for b, (d,t) in enumerate(dlTr):"
-FOR_EPOCH    = "for epoch in range(epochs):"
-DATA_TO_1D   = "d = d[:].reshape(1,-1)"
-DATA_TO_CUDA = "d, t = d.cuda(), t.cuda()"
-CRITERION    = "criterion({}, {}"
-VALIDATION   = "for bval, (dval, tval) in enumerate(dlVal):"
-OPT_STEP     = "{}.step()"
-OPT_ZERO     = "{}.zero_grad()"
-
