d0a0440faaf00e849fa08825ef2094ef6eb87d80
Lyle Hubbard <lhubbard@pdx.edu>
2021-12-29 14:29:37 -0800

    writes now normalize output data between 0 and 255. function for writing files in place. json write.

--- vgcg/src/WriteModuleTemplate.py
+++ vgcg/src/WriteModuleTemplate.py
@@ -1,11 +1,19 @@
+import json
+
 import torch
 import torch.nn as nn
+
+import numpy as np
+
 import os
 import sys
+
 
 def touch(fl):
   with open(fl, "w") as f:
     f.write("")
+
+
 
 
 class LinearWrite(nn.Linear):
@@ -16,14 +24,16 @@ class LinearWrite(nn.Linear):
       print(f"added {write_loc} to current dir")
     self.file_w = write_loc
     self.mode = mode 
-  
-  
+ 
 
 
   def write(self, X): 
+    X_np = X.clone().detach().numpy()
+    X_colorspace_255 = ((X_np + np.abs(X_np.min()))/X_np.max()) * 255
+    X_colorspace_255.astype(np.uint8)
     if self.mode == "APPEND" :
       with open(self.file_w, "a") as f:
-        f.write(str(X)) 
+        f.write(str(X_colorspace_255)) 
     
     elif self.mode == "WRITE":
       with open(self.file_w, "w") as f:
@@ -35,17 +45,17 @@ class LinearWrite(nn.Linear):
     return out
 
 
-def generate_writeable(X, ins, write_loc, mode, include_imports: bool = False, is_in_torch: bool = True):
+def generate_writeable(name: str, ins: str, write_loc: str, mode: str, include_imports: bool = False, is_in_torch: bool = True):
   
   #module definition 
-  X_super = ("nn." + X if is_in_torch else "nn.Module")
-  start = "class " + X + "Write(" + X_super + "):\n"
+  name_super = ("nn." + name if is_in_torch else "nn.Module")
+  start = "class " + name + "Write(" + name_super + "):\n"
 
 
-  
+  id_check = "    if id is None:  raise ValueError(\"each writing module requires a unique id for payloading.  '" + name + "Write'  aborting...\")\n"
   #useful in case we are constructing a large file, dont want redundant import calls
   if include_imports: 
-    imports = "import torch\nimport torch.nn as nn\nimport torch.nn.functional as F\n\nimport os\n\n\n"
+    imports = "import torch\nimport torch.nn as nn\nimport torch.nn.functional as F\n\nimport numpy as np\nimport os\nimport json\n\n"
     start = imports + start
 
 
@@ -53,7 +63,7 @@ def generate_writeable(X, ins, write_loc, mode, include_imports: bool = False, i
   init_ins = (str(ins)+", " if ins is not None else "")
 
   #actual init string
-  init = "  def __init__(self," + init_ins + write_loc + ": str = \"local.txt\", " + mode + ": str = \"APPEND\", **kwargs):\n"
+  init = "  def __init__(self," + init_ins + "write_loc: str = \"" + write_loc + "\", mode: str = \"" + mode +"\", id  = None, **kwargs):\n"
 
   #super line to hook to pytorch
   _super = "    super().__init__(" + init_ins + "**kwargs)\n"
@@ -62,16 +72,21 @@ def generate_writeable(X, ins, write_loc, mode, include_imports: bool = False, i
   template_writeable = """
     self.file_w = write_loc
     self.mode = mode
-  
+    self.id = id
 
   def write(self, X): 
+    X_np = X.clone().detach().numpy()
+    X_min, X_max = np.abs(X_np[0].min()), np.abs(X_np[0].max())
+    X_colorspace_255 = (X_np[0] + X_min)/(X_max + X_min) * 255
+    X_colorspace_255 = X_colorspace_255.astype(np.uint8)
     if self.mode == "APPEND" :
+
       with open(self.file_w, "a") as f:
-        f.write(str(X)) 
-    
+        json.dump({"data": str(X_colorspace_255), "id": self.id}, f) #originally was f.write
+        f.write(\"\\n\") 
     elif self.mode == "WRITE":
       with open(self.file_w, "w") as f:
-        f.write(str(X))
+        f.write(str(X_colorspace_255))
   
   def forward(self, X):
     out = super().forward(X)
@@ -79,26 +94,27 @@ def generate_writeable(X, ins, write_loc, mode, include_imports: bool = False, i
     return out
 
   """
-  return start + init + _super + template_writeable #constructs a module that writes its output upon activation
+  return start + init + _super + id_check + template_writeable #constructs a module that writes its output upon activation
 
 
 
 
 
-
-
-
+def write_generate_writeable(name, ins, write_loc, mode : str= "APPEND", include_imports: bool = False, is_in_torch: bool = True):
+  write_model = generate_writeable(name = name, ins = ins, write_loc = write_loc, mode = mode, include_imports = include_imports, is_in_torch = is_in_torch)
+  with open(name + "Write.py", "w") as f:
+    f.write(write_model)
+  print("wrote ", write_model)
 
 if __name__ == "__main__":
   print("testing " + __file__ + "...\n")
-  local = nn.Sequential(LinearWrite(100, 50), nn.ReLU(), LinearWrite(50, 10), nn.Softmax())
+  write_generate_writeable("Linear", ins = "in_features, out_features", write_loc = "local.txt", include_imports = True)
+  write_generate_writeable("ReLU", ins = None, write_loc = "local.txt", include_imports = True)
+  from LinearWrite import LinearWrite
+  from ReLUWrite import ReLUWrite
+  local = nn.Sequential(LinearWrite(100, 50, id="L1"), ReLUWrite(id = "R1"), LinearWrite(50, 10, id="L2"), nn.Softmax())
   D = torch.randn(1,1,100)
-
   print(D)
   local(D)
+  
 
-
-  write_model = generate_writeable("ReLU", None, "local.txt", "APPEND", include_imports = True)
-  with open("other.txt", "w") as f:
-    f.write(write_model)
-print(write_model)
