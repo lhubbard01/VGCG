61c256d8683a3b3e1c8f8390dea039626ece7ba9
Lyle Hubbard III <lhubbard@pdx.edu>
2023-02-27 11:07:07 -0800

    Update IPC.py
    code formatting in IPC

--- vgcg/src/IPC.py
+++ vgcg/src/IPC.py
@@ -5,28 +5,19 @@ import logging
 import select
 import signal
 import re
+import datetime as dt
+from loggingConf import CountLogger, init_logger, VERBOSE
 
 
 #designators for the interprocess communication via pipes with node server (bidirectional)
 IPC_FIFO_NAME_A = "pipe_a"
 IPC_FIFO_NAME_B = "pipe_b"
-import datetime as dt
-
-
-
-
-
-
-
-
 IPC_FIFO_MODEL = "model_pipe"
 
-from loggingConf import CountLogger, init_logger, VERBOSE
+VERBOSE = False
 
 logger = init_logger(__file__, count = 1)
 def prepend(*args):
-
-
   PREPEND = time.asctime() + " | PYTHON  [MAIN]:"
   for arg in args:
     PREPEND += str(arg)
@@ -35,34 +26,7 @@ def prepend(*args):
 
 logger.info = prepend
 
-"""
-class FifoModel:
-  def __init__(self, loc):
-    self.loc = loc
-    try:
-      self.pipe =  os.open(os.path.join(self.loc, IPC_FIFO_MODEL), os.O_WRONLY)
-    except Exception as e:
-      raise e
-  
-    self.write(msg)
 
-  def write(self, msg):
-    if not isinstance(msg, bytes):
-      if not isinstance(msg, str):
-        os.write(self.pipe, bytes(str(msg),"utf-8"))
-      else:
-        os.write(self.pipe, bytes(msg,"utf-8"))
-    else:
-      os.write(self.pipe, msg)
-  
-
-  def exit(self):
-    try:
-      os.remove(os.path.join(self.loc,IPC_FIFO_MODEL))
-    except FileNotFoundError as fnfe:
-      logger.critical(fnfe)
-
-  """  
 class FifoModel:
   def __init__(self, PIPE):
     self.pipe = PIPE
@@ -71,7 +35,6 @@ class FifoModel:
     self.write(msg)
 
   def write(self, msg : (bytes, str)):
-    global VERBOSE
     if VERBOSE :
       logger.info("MESSAGE: ", type(msg), msg, "\nPIPE: ", type(self.pipe))
     if not isinstance(msg, bytes):
@@ -93,14 +56,8 @@ def moveToModel(msg):
 
 exec_history = []
 symtable = {}
-VERBOSE = False
 def log_input(msg, *args):
-  
-  
-  
 
-
-  global VERBOSE
   if VERBOSE:
     print(msg, args)
   #global logger
@@ -112,11 +69,9 @@ def log_input(msg, *args):
   return msg
 
 def handle_message_runtime( msg_dict):
-  global exec_history, symtable #, logger
   msg, outbound = msg_dict["data"], None
   l_r = msg.split("=")
-  
-
+ 
   l_r[0] = l_r[0].replace(" ","")
   try:
     exec(msg, globals(), locals())
@@ -125,7 +80,7 @@ def handle_message_runtime( msg_dict):
       outbound = symtable[l_r[0]]
   except NameError as ne:
     try:
-      print(symtable); print(msg.replace(" ",""))
+      print(symtable, msg.replace(" ",""))
       outbound = symtable[msg]
     except KeyError as ke:    outbound = str(ke)
     except SyntaxError as se: outbound = str(se) 
@@ -162,16 +117,13 @@ def process_message(msg):
     msgdict = None
     try: 
       msgdict = json.loads(msg)
-
       logging.debug(msgdict)
-
-
     except Exception as e:
       raise e
+      
     msgstr = bytes_to_str(msg)
     logging.debug(msgstr)
     
-
     out1 = log_input(msgdict, "INBOUND")
     if msgdict["route"] == "model":
       out1 = moveToModel(msg)
@@ -180,17 +132,13 @@ def process_message(msg):
     elif msgdict["route"] == "pyexec":
       out1  = handle_message_runtime(msgdict)
       logging.info("handle interpreter state pyexec") 
-
+    
     log_input(out1, "OUTBOUND")
-   
     return str(out1)
 
-
   except Exception as e:
-    raise e
     logging.critical(e)
     raise e
-
 
 
 def pend_on_pipe(loc, name, F_flags):
@@ -203,45 +151,21 @@ def pend_on_pipe(loc, name, F_flags):
       pass
 
 
-
 class IPC_Handler:
   def __init__(self, loc):
     global fifo_model, logger
     if not logger: 
       logger = init_logger()
     
-    
     os.mkfifo(IPC_FIFO_NAME_A)
-    global VERBOSE
     try:
       fifo_a = os.open(os.path.join(loc, IPC_FIFO_NAME_A), os.O_RDONLY | os.O_NONBLOCK)
       logger.info("pipe a is opened")
-
-
-
       fifo_b     = pend_on_pipe(loc, IPC_FIFO_NAME_B, os.O_WRONLY)
       logger.info("pipe b is opened")
-
       fifo_model = FifoModel(pend_on_pipe(loc, IPC_FIFO_MODEL, os.O_WRONLY))
       logger.info("model pipe is opened")
 
-
-      """
-      while True:
-        try:
-          fifo_b = os.open(os.path.join(loc, IPC_FIFO_NAME_B), os.O_WRONLY)
-          logging.info("Pipe B is opened")
-          break
-        except:
-          pass
-      while True:
-        try:
-          fifo_model = FifoModel(loc)
-          print("model fifo is opened")
-          break
-        except:
-          pass
-      #Polling A"""
       try:
         logger.info("initiating pipe polling")
         poll = select.poll()
@@ -257,34 +181,26 @@ class IPC_Handler:
   
               if VERBOSE: logger.info("-------writing------", outbound)
               os.write(fifo_b, bytes(str(outbound),"utf-8"))
-              
-
-        
-
 
         except KeyboardInterrupt:
           pass
-
         finally:
           logger.info("select poll unregistering pipe a")
           #handles condition where a launching script calls this instead
           try:
             poll.unregister(fifo_a)
-
           except FileNotFoundError as fnfe :
             logger.info("cannot unregister selecting of pipe a due to pipe already being freed");
-
-
-
+      
       finally:
         logger.info("os closing fifo a pipe") 
         #handles condition where a launching script calls this instead
         try:
           os.close(fifo_a)
         except FileNotFoundError as fnfe :
-          logger.info("cannot close pipe a due to pipe already being freed");
-    finally:
-        
+          logger.info("cannot close pipe a due to pipe already being freed")
+          
+    finally:   
       #handles condition where a launching script calls this instead
       try:
         logger.info("OS removing IPC FIFO PIPES")
